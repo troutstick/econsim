a9195d0249d4ee3ce7c888204b02710dce9ea566
Justin Thein <jthein@berkeley.edu>
2020-02-18 13:14:31 -0800

    start of inventory class; nested in pop

--- src/market/Pop.java
+++ src/market/Pop.java
@@ -4,7 +4,8 @@ package market;
  *  Can buy and sell things. What a guy.
  *  */
 public class Pop {
-    private String _name, _id;
+    private String _name;
+    private int _id;
     private double _money;
     private Marketplace _marketplace;
     private boolean _isBankrupt;
@@ -12,12 +13,31 @@ public class Pop {
     /** Times to warn pop before it's bankrupt. */
     private int _bankruptcyThreshold;
 
-    public Pop(String name, String id, double money) {
+    private Inventory _inventory;
+
+    public Pop(String name, int id, double money) {
         _name = name;
         _id = id;
         _money = money;
         _isBankrupt = false;
         _marketplace = null;
         _bankruptcyThreshold = 5;
+    }
+
+    public class Inventory {
+        private Inventory() {
+
+        }
+
+        public Inventory getInventory(Pop pop) {
+            if (pop._inventory == null) {
+                pop._inventory = new Inventory();
+            }
+            return pop._inventory;
+        }
+
+        public void addGoods() {
+
+        }
     }
 }
