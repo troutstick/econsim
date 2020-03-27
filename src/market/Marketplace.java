6d9f20004c6448c280bfbfed906f4c162ae2a3ea
Justin Thein <jthein@berkeley.edu>
2020-03-26 18:25:05 -0700

    marketplace now has basic methods

--- src/market/Marketplace.java
+++ src/market/Marketplace.java
@@ -1,4 +1,26 @@
 package market;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Marketplace {
+
+    final String NAME;
+
+    /** The number of rounds used to calculate rolling avgs for this market's stats. */
+    final int ROLLING_AVG_WINDOW = 10;
+
+    /** The Pops that are in this marketplace. */
+    List<Pop> _agents;
+
+    Marketplace(String name, List<Pop> agents) {
+        NAME = name;
+        _agents = new ArrayList<>();
+        _agents.addAll(agents);
+    }
+
+    /** How many Pops live here. */
+    int population() {
+        return _agents.size();
+    }
 }
