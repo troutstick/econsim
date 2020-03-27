abf7a2aeef89ae0d710804f3c6c3b09ee6642e11
Justin Thein <jthein@berkeley.edu>
2020-03-27 15:29:50 -0700

    add money field to market

--- src/market/Marketplace.java
+++ src/market/Marketplace.java
@@ -10,6 +10,9 @@ public class Marketplace {
     /** The number of rounds used to calculate rolling avgs for this market's stats. */
     final int ROLLING_AVG_WINDOW = 10;
 
+    /** What kind of currency is used to buy/sell goods at this marketplace. */
+    private Money _money;
+
     /** The Pops that are in this marketplace. */
     List<Pop> _agents;
 
@@ -23,4 +26,7 @@ public class Marketplace {
     int population() {
         return _agents.size();
     }
+
+    /** Check what kind of currency is traded at this marketplace. */
+    Money money() { return _money; }
 }
