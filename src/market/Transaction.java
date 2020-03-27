e66bd4a0c0af78047bfb2359014b939be970dabc
Justin Thein <jthein@berkeley.edu>
2020-03-26 17:10:33 -0700

    add get methods to transaction

--- src/market/Transaction.java
+++ src/market/Transaction.java
@@ -2,16 +2,39 @@ package market;
 
 /** Represents an exchange of a resource for money. */
 public class Transaction {
-    final Resource _r;
-    final int _rAmount;
-    final Money _m;
-    final double _mAmount;
+    private final Resource _r;
+    private final int _rAmount;
+    private final Money _m;
+    private final double _mAmount;
+    private final Pop _buyer, _seller;
 
     /** Trade RAMOUNT of R for MAMOUNT of M. */
-    Transaction(Resource r, int rAmount, Money m, double mAmount) {
+    Transaction(Pop buyer, Pop seller, Resource r, int rAmount, Money m, double mAmount) {
         _r = r;
         _rAmount = rAmount;
         _m = m;
         _mAmount = mAmount;
+        _buyer = buyer;
+        _seller = seller;
+    }
+
+    /** Get the resource that was traded in the transaction. */
+    Resource resource() {
+        return _r;
+    }
+
+    /** Get the type of currency in the transaction. */
+    Money money() {
+        return _m;
+    }
+
+    /** How much of the resource was bought. */
+    int amountBought() {
+        return _rAmount;
+    }
+
+    /** How much money was used to buy in the transaction. */
+    double moneyAmount() {
+        return _mAmount;
     }
 }
